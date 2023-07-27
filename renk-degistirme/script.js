88abeecf5bd0ca30a879892311d2e04dc65dd951
alper0karaca <karacaalper806@gmail.com>
2023-07-27 16:15:06 +0300

    change background

---
+++ renk-degistirme/script.js
@@ -0,0 +1,12 @@
+function changeBackground(){
+
+    const inputcolorpicker = document.getElementById("colorpicker-input");
+
+    const selected = inputcolorpicker.value;
+
+    document.body.style.backgroundColor = selected;
+
+}
+
+
+
